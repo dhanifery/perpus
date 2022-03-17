35063e2183bcd2049482d5b8c8a8a01f32e68073
dhanifery <feryr4@gmail.com>
2022-03-18 00:06:19 +0700

    Tugas Pertemuan 1

---
+++ application/controllers/Login.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->model('m_login');
+	}
+
+	public function index()
+	{
+		$this->load->view('login/v_login');
+	}
+
+	public function proses_login()
+	{
+		$user = $this->input->post('username');
+		$pass = $this->input->post('password');
+		$this->m_login->proses_login($user,$pass);
+	}
+}
---
+++ application/models/M_login.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_login extends CI_Model {
+
+	public function proses_login($user,$pass)
+	{
+          $password = md5($pass);
+          $user = $this->db->where('username',$user);
+          $pass = $this->db->where('password',$password);
+          $query = $this->db->get('login');
+          if ($query->num_rows()>0) {
+               foreach ($query->result() as $row) {
+                    $sess = array(
+                         'id'           => $row->id,
+                         'nama'         => $row->nama,
+                         'username'     => $row->username,
+                         'password'     => $row->password,
+                         'level'        => $row->level
+                    );
+                    $this->session->set_userdata($sess);
+               }
+               redirect('Dashboard');
+          }
+          else
+          {
+               $this->session->set_flashdata('info', '<div class="alert alert-danger" role="alert">Login Gagal, Silahkan periksa username dan password !</div>');
+               redirect('login');
+          }
+	}
+}
